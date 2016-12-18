7c744dfe65c1a464b7621494810b43cd67af6111
Tim Matthews <tim.matthews7@gmail.com>
2016-12-18 18:41:12 +1300

    Added another example to show method overloading

--- src/Main.hs
+++ src/Main.hs
@@ -5,20 +5,30 @@ module Main where
 import Clr
 import Clr.Host
 
-import Foreign.C.String
-import Foreign.Ptr
+import Data.Int(Int32)
+import Foreign.C.String(CString)
+import Foreign.Ptr(FunPtr)
 
 instance MethodS1 (T "System.Console" '[]) (T "WriteLine" '[]) (T "System.String" '[]) where
   type ResultTypeS1 (T "System.Console" '[]) (T "WriteLine" '[]) (T "System.String" '[]) = 'Nothing
-  rawInvokeS1 x = stub >>= \f-> f x >> return ()
+  rawInvokeS1 x = stub1 >>= \f-> f x >> return ()
 
-type WLT = CString -> IO ()
-foreign import ccall "dynamic" makeWLT :: FunPtr WLT -> WLT
+foreign import ccall "dynamic" makeWriteLineType1 :: FunPtr (CString -> IO ()) -> (CString -> IO ())
 
-stub :: IO WLT
-stub = getMethodStub "System.Console, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" "WriteLine" "System.String" >>= return . makeWLT
+stub1 :: IO (CString -> IO ())
+stub1 = getMethodStub "System.Console, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" "WriteLine" "System.String" >>= return . makeWriteLineType1
+
+instance MethodS1 (T "System.Console" '[]) (T "WriteLine" '[]) (T "System.Int32" '[]) where
+  type ResultTypeS1 (T "System.Console" '[]) (T "WriteLine" '[]) (T "System.Int32" '[]) = 'Nothing
+  rawInvokeS1 x = stub2 >>= \f-> f x >> return ()
+
+foreign import ccall "dynamic" makeWriteLineType2 :: FunPtr (Int32 -> IO ()) -> (Int32 -> IO ())
+
+stub2 :: IO (Int32 -> IO ())
+stub2 = getMethodStub "System.Console, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" "WriteLine" "System.Int32" >>= return . makeWriteLineType2
 
 main :: IO ()
 main = do
   startClr
   invokeS @"WriteLine" @"System.Console" "Hello CLR!!!"
+  invokeS @"WriteLine" @"System.Console" (2 :: Int32)   -- overloaded
