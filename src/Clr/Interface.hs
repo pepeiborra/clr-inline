36ad8cc3892f1b25e291a895a01019c80c1a9a28
Tim Matthews <tim.matthews7@gmail.com>
2016-12-29 12:58:43 +1300

    Added temporary interface declarations so that families always reduce

--- src/Clr/Interface.hs
+++ src/Clr/Interface.hs
@@ -15,4 +15,23 @@ type family Interfaces (t::Type) :: [Type]
 type family Implements (t::Type) (i::Type) :: Bool where
   Implements t i = i `Elem` (Interfaces t)
 
+-- TODO: temporary instances here
+type instance Interfaces (T "System.Object" '[])    = '[]
+type instance Interfaces (T "System.ValueType" '[]) = '[]
+
+type instance Interfaces (T "System.String"  '[]) = '[]
+type instance Interfaces (T "System.SByte"   '[]) = '[]
+type instance Interfaces (T "System.Byte"    '[]) = '[]
+type instance Interfaces (T "System.Int16"   '[]) = '[]
+type instance Interfaces (T "System.UInt16"  '[]) = '[]
+type instance Interfaces (T "System.Int32"   '[]) = '[]
+type instance Interfaces (T "System.UInt32"  '[]) = '[]
+type instance Interfaces (T "System.Int64"   '[]) = '[]
+type instance Interfaces (T "System.UInt64"  '[]) = '[]
+type instance Interfaces (T "System.IntPtr"  '[]) = '[]
+type instance Interfaces (T "System.UIntPtr" '[]) = '[]
+type instance Interfaces (T "System.Char"    '[]) = '[]
+type instance Interfaces (T "System.Single"  '[]) = '[]
+type instance Interfaces (T "System.Double"  '[]) = '[]
+
 
