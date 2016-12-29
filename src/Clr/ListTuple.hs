60dfb8cb23233b624e0c841e57a4a334782e140c
Tim Matthews <tim.matthews7@gmail.com>
2016-12-29 16:03:06 +1300

    Added TupleToList, simplified ListSize, and extra equations for other closed tfs

--- src/Clr/ListTuple.hs
+++ src/Clr/ListTuple.hs
@@ -13,17 +13,47 @@ import GHC.TypeLits
 -- List to tuple conversion
 --
 type family ListToTuple (t :: [Type]) :: Type where
-  ListToTuple '[]                                 = ()
-  ListToTuple (a ': '[])                         = (a)
-  ListToTuple (a ': b ': '[])                   = (a,b)
-  ListToTuple (a ': b ': c ': '[])             = (a,b,c)
-  ListToTuple (a ': b ': c ': d ': '[])       = (a,b,c,d)
-  ListToTuple (a ': b ': c ': d ': e ': '[]) = (a,b,c,d,e)
+  ListToTuple '[]                                                          = ()
+  ListToTuple (a ': '[])                                                   = (a)
+  ListToTuple (a ': b ': '[])                                              = (a,b)
+  ListToTuple (a ': b ': c ': '[])                                         = (a,b,c)
+  ListToTuple (a ': b ': c ': d ': '[])                                    = (a,b,c,d)
+  ListToTuple (a ': b ': c ': d ': e ': '[])                               = (a,b,c,d,e)
+  ListToTuple (a ': b ': c ': d ': e ': f ': '[])                          = (a,b,c,d,e,f)
+  ListToTuple (a ': b ': c ': d ': e ': f ': g ': '[])                     = (a,b,c,d,e,f,g)
+  ListToTuple (a ': b ': c ': d ': e ': f ': g ': h ': '[])                = (a,b,c,d,e,f,g,h)
+  ListToTuple (a ': b ': c ': d ': e ': f ': g ': h ': i ': '[])           = (a,b,c,d,e,f,g,h,i)
+  ListToTuple (a ': b ': c ': d ': e ': f ': g ': h ': i ': j ': '[])      = (a,b,c,d,e,f,g,h,i,j)
+  ListToTuple (a ': b ': c ': d ': e ': f ': g ': h ': i ': j ': k ': '[]) = (a,b,c,d,e,f,g,h,i,j,k)
+
+--
+-- Tuple to list conversion
+--
+type family TupleToList (t::Type) :: [Type] where
+  TupleToList ()                      = '[]
+  TupleToList (a,b)                   = '[a, b]
+  TupleToList (a,b,c)                 = '[a, b, c]
+  TupleToList (a,b,c,d)               = '[a, b, c, d]
+  TupleToList (a,b,c,d,e)             = '[a, b, c, d, e]
+  TupleToList (a,b,c,d,e,f)           = '[a, b, c, d, e, f]
+  TupleToList (a,b,c,d,e,f,g)         = '[a, b, c, d, e, f, g]
+  TupleToList (a,b,c,d,e,f,g,h)       = '[a, b, c, d, e, f, g, h]
+  TupleToList (a,b,c,d,e,f,g,h,i)     = '[a, b, c, d, e, f, g, h, i]
+  TupleToList (a,b,c,d,e,f,g,h,i,j)   = '[a, b, c, d, e, f, g, h, i, j]
+  TupleToList (a,b,c,d,e,f,g,h,i,j,k) = '[a, b, c, d, e, f, g, h, i, j, k]
+  TupleToList (a)                     = '[a]
 
 --
 -- Size of a tuple
 --
-type family TupleSize (x::k) :: Nat where
+type family TupleSize (x::tupleKind) :: Nat where
+  TupleSize (a,b,c,d,e,f,g,h,i,j,k) = 11
+  TupleSize (a,b,c,d,e,f,g,h,i,j) = 10
+  TupleSize (a,b,c,d,e,f,g,h,i) = 9
+  TupleSize (a,b,c,d,e,f,g,h) = 8
+  TupleSize (a,b,c,d,e,f,g) = 7
+  TupleSize (a,b,c,d,e,f) = 6
+  TupleSize (a,b,c,d,e) = 5
   TupleSize (a,b,c,d) = 4
   TupleSize (a,b,c) = 3
   TupleSize (a,b) = 2
@@ -33,10 +63,8 @@ type family TupleSize (x::k) :: Nat where
 -- Size of a list
 --
 type family ListSize (x::k) :: Nat where
-  ListSize '[a,b,c,d] = 4
-  ListSize '[a,b,c] = 3
-  ListSize '[a,b] = 2
-  ListSize '[a] = 1
+  ListSize    '[]    = 0
+  ListSize (x ': xs) = 1 + (ListSize xs)
 
 --
 -- a `Elem` xs is true if a is contained within xs
