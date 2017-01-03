f6435ec283934ef6e4906762cf06a350ce23b9b2
Tim Matthews <tim.matthews7@gmail.com>
2017-01-03 19:15:08 +1300

    Changes due to how string are now marshaled

--- src/Main.hs
+++ src/Main.hs
@@ -14,14 +14,15 @@ import Clr.StaticMethod
 import Clr.Constructor
 
 import Data.Int(Int32, Int64)
-import Foreign.C.String(CString)
-import Foreign.Ptr(FunPtr)
+import Data.Word(Word16)
+import Foreign.Ptr(Ptr, FunPtr)
+
 
 instance MethodS1 (T "System.Console" 'Nothing '[]) (T "WriteLine" 'Nothing '[]) (T "System.String" 'Nothing '[]) where
   type ResultTypeS1 (T "System.Console" 'Nothing '[]) (T "WriteLine" 'Nothing '[]) (T "System.String" 'Nothing '[]) = 'Nothing
   rawInvokeS1 x = getMethodStub "System.Console, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" "WriteLine" "System.String" >>= return . makeWriteLineType1 >>= \f-> f x
 
-foreign import ccall "dynamic" makeWriteLineType1 :: FunPtr (CString -> IO ()) -> (CString -> IO ())
+foreign import ccall "dynamic" makeWriteLineType1 :: FunPtr (Ptr Word16 -> IO ()) -> (Ptr Word16 -> IO ())
 
 instance MethodS1 (T "System.Console" 'Nothing '[]) (T "WriteLine" 'Nothing '[]) (T "System.Int32" 'Nothing '[]) where
   type ResultTypeS1 (T "System.Console" 'Nothing '[]) (T "WriteLine" 'Nothing '[]) (T "System.Int32" 'Nothing '[]) = 'Nothing
@@ -33,13 +34,13 @@ instance MethodS2 (T "System.Console" 'Nothing '[]) (T "WriteLine" 'Nothing '[])
   type ResultTypeS2 (T "System.Console" 'Nothing '[]) (T "WriteLine" 'Nothing '[]) (T "System.String" 'Nothing '[]) (T "System.Object" 'Nothing '[]) = 'Nothing
   rawInvokeS2 x y = getMethodStub "System.Console, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" "WriteLine" "System.String;System.Object" >>= return . makeWriteLineType3 >>= \f-> f x y
 
-foreign import ccall "dynamic" makeWriteLineType3 :: FunPtr (CString -> (ObjectID a) -> IO ()) -> (CString -> (ObjectID a) -> IO ())
+foreign import ccall "dynamic" makeWriteLineType3 :: FunPtr (Ptr Word16 -> (ObjectID a) -> IO ()) -> (Ptr Word16 -> (ObjectID a) -> IO ())
 
 instance MethodS3 (T "System.Console" 'Nothing '[]) (T "WriteLine" 'Nothing '[]) (T "System.String" 'Nothing '[]) (T "System.Object" 'Nothing '[]) (T "System.Object" 'Nothing '[]) where
   type ResultTypeS3 (T "System.Console" 'Nothing '[]) (T "WriteLine" 'Nothing '[]) (T "System.String" 'Nothing '[]) (T "System.Object" 'Nothing '[]) (T "System.Object" 'Nothing '[]) = 'Nothing
   rawInvokeS3 x y z = getMethodStub "System.Console, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" "WriteLine" "System.String;System.Object;System.Object" >>= return . makeWriteLineType4 >>= \f-> f x y z
 
-foreign import ccall "dynamic" makeWriteLineType4 :: FunPtr (CString -> (ObjectID a) -> (ObjectID b) -> IO ()) -> (CString -> (ObjectID a) -> (ObjectID b) -> IO ())
+foreign import ccall "dynamic" makeWriteLineType4 :: FunPtr (Ptr Word16 -> (ObjectID a) -> (ObjectID b) -> IO ()) -> (Ptr Word16 -> (ObjectID a) -> (ObjectID b) -> IO ())
 
 
 
@@ -47,7 +48,6 @@ type instance Candidates (T "System.Console" 'Nothing '[]) (T "WriteLine" 'Nothi
                                                                                            , '[ T "System.Int32" 'Nothing '[] ]
                                                                                            , '[ T "System.String" 'Nothing '[], T "System.Object" 'Nothing '[] ]
                                                                                            , '[ T "System.String" 'Nothing '[], T "System.Object" 'Nothing '[], T "System.Object" 'Nothing '[] ] ]
-
 main :: IO ()
 main = do
   startClr
