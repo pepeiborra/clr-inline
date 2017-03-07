0f9dc2c42c543121c2cfbb3720f71b92d075d38c
Pepe Iborra <pepeiborra@gmail.com>
2017-03-07 22:09:22 +0000

    Refactored all the non F# specific bits

--- packages/clr-inline/src/Clr/FSharp/Gen.hs
+++ packages/clr-inline/src/Clr/FSharp/Gen.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE RecordWildCards #-}
-module Clr.FSharp.Gen (FSharpUnit(..), FSharpModule(..), compile) where
+module Clr.FSharp.Gen (compile) where
 
 import           Clr.Inline.Utils
+import           Clr.Inline.Types
 import qualified Data.ByteString     as BS
 import           Data.List
 import           Data.Typeable
@@ -12,38 +13,30 @@ import           System.IO.Temp
 import           System.Process
 import           Text.Printf
 
-data FSharpUnit = FSharpUnit
-  { name, body  :: String
-  , args        :: [String]
-  } deriving (Show, Typeable)
-
-data FSharpModule = FSharpModule
-  { moduleName      :: String
-  , moduleNamespace :: String
-  , moduleUnits     :: [FSharpUnit]}
-
 -- The name of the F# compiler in Mono.
 -- TODO support both "fsc" and "fsharpc"
 fsharp = "fsharpc"
 
-genCode :: FSharpModule -> String
-genCode FSharpModule{..} =
+data FSharp
+
+genCode :: ClrInlinedGroup FSharp -> String
+genCode ClrInlinedGroup{..} =
   unlines $
-    [printf "namespace %s" moduleNamespace
+    [printf "namespace %s" modNamespace
     ,"open System" -- TODO imports
-    ,printf "module %s = " moduleName
+    ,printf "module %s = " modName
     ] ++ concat
-    [ printf "    let %s (%s) = " name (intercalate ", " args) :
+    [ printf "    let %s (%s) = " name (intercalate ", " argTypes) :
       [ printf "        %s" l | l <- lines body ]
-    | FSharpUnit{..} <- moduleUnits
+    | ClrInlinedUnit{..} <- units
     ]
 
-compile :: FSharpModule -> IO ClrBytecode
-compile m@FSharpModule {..} = do
+compile :: ClrInlinedGroup FSharp -> IO ClrBytecode
+compile m@ClrInlinedGroup {..} = do
     temp <- getTemporaryDirectory
     dir <- createTempDirectory temp "inline-clr"
-    let src = dir </> moduleName <.> ".fs"
-        tgt = dir </> moduleName <.> ".dll"
+    let src = dir </> modName <.> ".fs"
+        tgt = dir </> modName <.> ".dll"
     writeFile src (genCode m)
     putStrLn $ "Generated " ++ tgt
     callProcess fsharp ["--target:library", "--out:"++tgt, src]
--- packages/clr-inline/src/Clr/FSharp/Inline.hs
+++ packages/clr-inline/src/Clr/FSharp/Inline.hs
@@ -5,12 +5,13 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
 
-module Clr.FSharp.Inline (fsharp) where
+module Clr.FSharp.Inline (fsharp, getMethodStub, FunPtr) where
 
 import Control.Monad.IO.Class
 import Clr.Bindings
 import Clr.Inline.State
 import Clr.Inline.Utils
+import Clr.Inline.Types
 import Clr.FSharp.Gen
 import Language.Haskell.TH
 import Language.Haskell.TH.Quote
@@ -18,7 +19,6 @@ import Language.Haskell.TH.Syntax
 import Foreign
 import Text.Printf
 
-namespace = "Clr.FSharp.Inline"
 
 fsharp = QuasiQuoter
     { quoteExp  = fsharpExp
@@ -27,56 +27,9 @@ fsharp = QuasiQuoter
     , quoteDec  = error "Clr.FSharp.Inline: quoteDec"
     }
 
--- | Runs after the whole module has been loaded and is responsible for generating:
---     - A clr assembly with all the inline code, embedding it into the module.
-genFSharp = do
-  FinalizerState {wrappers} <- getFinalizerState @ FSharpUnit
-  modName <- mangleModule <$> thisModule
-  let mod = FSharpModule modName namespace wrappers
-  result <- runIO $ compile mod
-
-  -- Embed the bytecodes
-  embedBytecode =<< runIO(compile mod)
-  return ()
-
 -- | Quasiquoter for expressions. Responsible for:
 --      - Installing a finalizer to generate the bytecodes
 --      - Generating the foreign import wrapper.
 --      - Splicing in the computation that loads the bytecodes, gets a function pointer through the keyhole, and calls it.
 fsharpExp :: String -> Q Exp
-fsharpExp body = do
-  methodName <- (\d -> "quote_" ++ show d) <$> getFinalizerCount @ FSharpUnit
-  stubName <- newName "stub"
-  -- TODO support for antiquotations
-  let args = [] :: [Q Exp]
-  let argTypes = [] :: [String]
-  modName <- mangleModule <$> thisModule
-  let assemblyName = modName
-  pushWrapperGen genFSharp $ return (FSharpUnit methodName body argTypes)
-  let fullClassName :: String =
-        printf
-          "%s.%s, %s, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null"
-          namespace
-          modName
-          assemblyName
-  -- Generate the top level foreign import calls needed to convert the function pointers into Haskell functions
-  -- TODO Add support for argument and result type inference
-  resTy <- [t|IO ()|]
-  let argTys = []
-  let funTy = foldr AppT resTy argTys
-  lookupTypeName "Foreign.Ptr.FunPtr" >>= \case
-    Nothing -> error "Please import Foreing.Ptr when using F# quotations"
-    Just funPtrTy ->
-        qAddTopDecls
-          [ ForeignD
-              (ImportF
-                CCall
-                Safe
-                "dynamic"
-                stubName
-                (ArrowT `AppT` AppT (ConT funPtrTy) funTy `AppT` funTy))
-          ]
-  -- splice in the bytecode load and call to the stub
-  [|unembedBytecode >>
-    getMethodStub $(lift fullClassName) $(lift methodName) $(lift argTypes) >>=
-    return . $(varE stubName) >>= \f -> $(foldr appE [|f|] args)|]
+fsharpExp = clrQuoteExp compile
