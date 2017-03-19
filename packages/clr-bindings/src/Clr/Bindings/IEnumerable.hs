05485ea7db942258b29902d8fa7f2507bfff42d6
Tim Matthews <tim.matthews7@gmail.com>
2017-03-19 19:47:44 +1300

    Conversion of IEnumerable to Pipes Producer working with example

--- packages/clr-bindings/src/Clr/Bindings/IEnumerable.hs
+++ packages/clr-bindings/src/Clr/Bindings/IEnumerable.hs
@@ -1,18 +1,35 @@
-{-# LANGUAGE TypeInType, TypeApplications, FlexibleInstances, MultiParamTypeClasses, TypeFamilies #-}
+{-# LANGUAGE GADTs, DataKinds, KindSignatures, PolyKinds, TypeFamilies, TypeOperators, RankNTypes, MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances, FlexibleContexts, ScopedTypeVariables, ExistentialQuantification #-}
+{-# LANGUAGE UndecidableInstances, TypeApplications, AllowAmbiguousTypes, TypeInType, TypeFamilyDependencies, FunctionalDependencies #-}
+
 
 module Clr.Bindings.IEnumerable where
 
 import Clr
-import Clr.Host
-import Clr.Bindings.Host
-import Foreign.Ptr
+import Clr.Bridge
+import Clr.Marshal
 
-type T_IEnumerable t = T "System.Collectios.Generic.IEnumerable" '[t]
-type T_IEnumerator t = T "System.Collectios.Generic.IEnumerator" '[t]
+import Clr.Host
+import Clr.Host.BStr
+
+import Clr.Bindings.Host
+
+import Data.Kind
+import Data.Type.Bool
+import Data.Type.Equality
+import Foreign.Ptr
+import GHC.TypeLits
+
+import Pipes
+
+type T_IEnumerable t = T "System.Collections.Generic.IEnumerable" '[t]
+type T_IEnumerator t = T "System.Collections.Generic.IEnumerator" '[t]
+
+type instance SuperTypes (T_IEnumerable t) = '[ T "System.Object" '[] ]
 
 instance MethodI1 (T_IEnumerable t) (T "GetEnumerator" '[]) () where
   type ResultTypeI1 (T_IEnumerable t) (T "GetEnumerator" '[]) () = 'Just (T_IEnumerator t)
-  rawInvokeI1 ienumerable _ = getMethodStub "System.Collections.Generic.IEnumerable`1, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" "GetEnumerator" "" >>= return . makeGetEnumerator >>= \f-> f ienumerable
+  rawInvokeI1 ienumerable () = getMethodStub "System.Collections.Generic.IEnumerable`1[System.String], mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" "GetEnumerator" "" >>= return . makeGetEnumerator >>= \f-> f ienumerable
 
 foreign import ccall "dynamic" makeGetEnumerator :: FunPtr (ObjectID a -> IO (ObjectID b)) -> (ObjectID a -> IO (ObjectID b))
 
@@ -20,5 +37,75 @@ type instance Members (T_IEnumerable t) = '[ T "GetEnumerator" '[] ]
 
 type instance Candidates (T_IEnumerable t) (T "GetEnumerator" '[]) = '[ '[] ]
 
-getEnumerator :: Object (T_IEnumerable t) -> IO (Object (T_IEnumerator t))
-getEnumerator x = invokeI @"GetEnumerator" x ()
+getEnumerator :: forall t elem . ((t `Implements` (T_IEnumerable elem)) ~ 'True, IEnumElemT t ~ elem) => Object t -> IO (Object (T_IEnumerator elem))
+getEnumerator x = getEnumerator' $ upCast x
+  where getEnumerator' :: Object (T_IEnumerable elem) -> IO (Object (T_IEnumerator elem))
+        getEnumerator' x = invokeI @"GetEnumerator" x ()
+
+type family IEnumElemT (x::Type) :: Type where
+  IEnumElemT t = IEnumElemT' (WithAllSuperTypes t)
+
+type family IEnumElemT' (x::[Type]) :: Type where
+  IEnumElemT'        '[]                                                   = TypeError (Text "Not an IEnumerable")
+  IEnumElemT' ((T "System.Collections.Generic.IEnumerable" '[elem]) ': xs) = elem
+  IEnumElemT'     (x ': xs)                                                = IEnumElemT' xs
+
+type T_Current = T "Current" '[]
+
+instance PropertyI (T_IEnumerator t) T_Current where
+  type PropertyTypeI (T_IEnumerator t) T_Current = t
+
+instance PropertyGetI (T_IEnumerator (T "System.String" '[])) T_Current where
+  rawGetPropI ienumerator = getMethodStub "System.Collections.Generic.IEnumerator`1[System.String], mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" "get_Current" "" >>= return . makeEnumeratorCurrent >>= \f-> f ienumerator
+
+type instance Members (T_IEnumerator t) = '[ T_Current, T_MoveNext ]
+
+foreign import ccall "dynamic" makeEnumeratorCurrent :: FunPtr (ObjectID (T_IEnumerator elem) -> IO BStr) -> (ObjectID (T_IEnumerator elem) -> IO BStr)
+
+ienumCurrent :: forall elem propertyBridge propertyHask .
+  ( PropertyGetI (T_IEnumerator elem) T_Current
+  , BridgeType (PropertyTypeI (T_IEnumerator elem) T_Current) ~ propertyBridge
+  , UnmarshalAs propertyBridge ~ propertyHask
+  , Unmarshal propertyBridge propertyHask
+  ) => Object (T_IEnumerator elem) -> IO propertyHask
+ienumCurrent ienum = getPropI @T_Current ienum
+
+type T_MoveNext = T "MoveNext" '[]
+
+type instance Candidates (T_IEnumerator t) T_MoveNext = '[ '[] ]
+
+instance MethodI1 (T_IEnumerator t) T_MoveNext () where
+  type ResultTypeI1 (T_IEnumerator t) T_MoveNext () = 'Just (T "System.Boolean" '[])
+  rawInvokeI1 ienumerator () = getMethodStub "System.Collections.IEnumerator, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" "MoveNext" "" >>= return . makeEnumeratorMoveNext >>= \f-> f ienumerator
+
+foreign import ccall "dynamic" makeEnumeratorMoveNext :: FunPtr (ObjectID (T_IEnumerator elem) -> IO Bool) -> (ObjectID (T_IEnumerator elem) -> IO Bool) 
+
+ienumMoveNext :: forall elem . Object (T_IEnumerator elem) -> IO Bool
+ienumMoveNext ienum = invokeI @T_MoveNext ienum ()
+
+toProducer :: forall t elem elemBridge elemHask .
+  ( (t `Implements` (T_IEnumerable elem)) ~ 'True
+  , IEnumElemT t ~ elem
+  , PropertyGetI (T_IEnumerator elem) T_Current
+  , BridgeType (PropertyTypeI (T_IEnumerator elem) T_Current) ~ elemBridge
+  , UnmarshalAs elemBridge ~ elemHask
+  , Unmarshal elemBridge elemHask
+  ) => Object t -> Producer elemHask IO ()
+toProducer ienumerable = do
+  ienumerator <- liftIO $ getEnumerator ienumerable
+  toProducer' ienumerator
+
+toProducer' :: forall elem elemBridge elemHask .
+  ( PropertyGetI (T_IEnumerator elem) T_Current
+  , BridgeType (PropertyTypeI (T_IEnumerator elem) T_Current) ~ elemBridge
+  , UnmarshalAs elemBridge ~ elemHask
+  , Unmarshal elemBridge elemHask
+  ) => Object (T_IEnumerator elem) -> Producer elemHask IO ()
+toProducer' ienumerator = do
+  nxt <- liftIO $ ienumMoveNext ienumerator
+  if nxt then do
+    cur <- liftIO $ ienumCurrent ienumerator
+    yield cur
+    toProducer' ienumerator
+  else return ()
+
--- packages/clr-test-app/src/Main.hs
+++ packages/clr-test-app/src/Main.hs
@@ -5,9 +5,13 @@ module Main where
 import Clr
 import Clr.Host
 import Clr.Bindings
+import Clr.Bindings.IEnumerable
 
 import Data.Int(Int32, Int64)
 import Foreign.Ptr(Ptr, FunPtr)
+
+import Pipes
+import Pipes.Prelude
 
 instance MethodS1 (T "System.Console" '[]) (T "WriteLine" '[]) () where
   type ResultTypeS1 (T "System.Console" '[]) (T "WriteLine" '[]) () = 'Nothing
@@ -39,13 +43,29 @@ instance MethodS3 (T "System.Console" '[]) (T "WriteLine" '[]) (T "System.String
 
 foreign import ccall "dynamic" makeWriteLineType4 :: FunPtr (BStr -> (ObjectID a) -> (ObjectID b) -> IO ()) -> (BStr -> (ObjectID a) -> (ObjectID b) -> IO ())
 
-
-
 type instance Candidates (T "System.Console" '[]) (T "WriteLine" '[]) = '[ '[                                                                     ]
                                                                          , '[ T "System.String" '[]                                               ]
                                                                          , '[ T "System.Int32"  '[]                                               ]
                                                                          , '[ T "System.String" '[], T "System.Object" '[]                        ]
                                                                          , '[ T "System.String" '[], T "System.Object" '[], T "System.Object" '[] ] ]
+
+type instance Candidates (T "System.Collections.Generic.List" '[t]) (T "System.Collections.Generic.List" '[t]) = '[ '[] ]
+
+instance Constructor1 (T "System.Collections.Generic.List" '[t]) () where
+  rawNew1 () = getMethodStub "System.Collections.Generic.List`1[System.String], mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" ".ctor" "" >>= return . makeListCTor >>= \f-> f
+
+foreign import ccall "dynamic" makeListCTor :: FunPtr (IO (ObjectID a)) -> IO (ObjectID a)
+
+type instance SuperTypes (T "System.Collections.Generic.List" '[t]) = '[ T "System.Collections.Generic.IEnumerable" '[t] ]
+
+type instance Members (T "System.Collections.Generic.List" '[t]) = '[ T "Add" '[] ]
+type instance Candidates (T "System.Collections.Generic.List" '[t]) (T "Add" '[]) = '[ '[t] ]
+
+instance MethodI1 (T "System.Collections.Generic.List" '[T "System.String" '[]]) (T "Add" '[]) (T "System.String" '[]) where
+  type ResultTypeI1 (T "System.Collections.Generic.List" '[T "System.String" '[]]) (T "Add" '[]) (T "System.String" '[]) = 'Nothing
+  rawInvokeI1 l s = getMethodStub "System.Collections.Generic.List`1[System.String], mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" "Add" "System.String" >>= return . makeListAdd >>= \f-> f l s
+
+foreign import ccall "dynamic" makeListAdd :: FunPtr (ObjectID a -> BStr -> IO ()) -> (ObjectID a -> BStr -> IO ())
 
 main :: IO ()
 main = do
@@ -55,3 +75,9 @@ main = do
   invokeS @"WriteLine" @"System.Console" (2 :: Int32)                           -- Console.WriteLine(Int32)
   invokeS @"WriteLine" @"System.Console" ("The year is {0}", 2017::Int64)       -- Console.WriteLine(String, Object)
   invokeS @"WriteLine" @"System.Console" ("Well {0} {1}", "This", "Is Cool")    -- Console.WriteLine(String, Object, Object)
+  list <- new @'("System.Collections.Generic.List", "System.String") ()
+  invokeI @"Add" list "foo"
+  invokeI @"Add" list "bar"
+  runEffect $ toProducer list >-> stdoutLn
+  return ()
+
