11c372b8cb87481583291ea9c0559e0c1ae1bb05
Tim Matthews <tim.matthews7@gmail.com>
2017-03-19 19:46:54 +1300

    String unmarshaling

--- packages/clr-bindings/src/Clr/Bindings/Marshal.hs
+++ packages/clr-bindings/src/Clr/Bindings/Marshal.hs
@@ -8,8 +8,12 @@ import Clr.Marshal
 
 import Clr.Host.BStr
 
+import Data.Coerce
 import Data.Text
 import Data.Text.Foreign
+import Data.Word
+import Foreign.Ptr
+import Foreign.Storable
 
 instance Marshal Text BStr where
   marshal x f = do
@@ -20,4 +24,17 @@ instance Marshal String BStr where
   marshal x f = marshal (pack x) f
 
 type instance BridgeTypePrim (T "System.String" '[]) = BStr
+
+instance Unmarshal BStr Text where
+  unmarshal x = do
+    let charSize = 2
+    let ptrData   = coerce x              :: Ptr Word16
+    let ptrLen    = plusPtr ptrData (-4)  :: Ptr Word16
+    lenBytes     <- peek ptrLen
+    fromPtr ptrData $ fromIntegral $ lenBytes `div` charSize
+
+instance Unmarshal BStr String where
+  unmarshal x = unmarshal x >>= return . unpack
+
+type instance UnmarshalAs BStr = String
 
