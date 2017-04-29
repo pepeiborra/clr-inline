f63ec084557810a44c3e848614b126775d35dd84
Tim Matthews <tim.matthews7@gmail.com>
2017-04-29 18:16:52 +1200

    All objects should come with the TString context of the type

--- libs/clr-typed/src/Clr/Object.hs
+++ libs/clr-typed/src/Clr/Object.hs
@@ -5,6 +5,7 @@ module Clr.Object where
 
 import Clr.Marshal
 import Clr.Types
+import Clr.TypeString
 import Data.Kind
 import Data.Int
 
@@ -18,7 +19,7 @@ newtype ObjectID typ = ObjectID Int64
 -- An object is just its unique identifer + information of its type
 --
 data Object (typ::Type) where
-  Object :: ObjectID typ -> Object typ
+  Object :: (TString typ) => ObjectID typ -> Object typ
 
 
 --
@@ -27,7 +28,7 @@ data Object (typ::Type) where
 instance {-# OVERLAPS #-} Marshal (Object t) (ObjectID t) where
   marshal (Object x) f = f x
 
-instance {-# OVERLAPPING #-} Unmarshal (ObjectID t) (Object t) where
+instance {-# OVERLAPPING #-} (TString t) => Unmarshal (ObjectID t) (Object t) where
   unmarshal oid = return $ Object oid
 
 type instance HaskToClr (Object t) = t
