508e784e934bf4a5e9ba5166e57066af005176a5
Tim Matthews <tim.matthews7@gmail.com>
2017-05-13 16:23:43 +1200

    Fixup the test suite for clr-typed

---
+++ libs/clr-typed/test/Instances.hs
@@ -0,0 +1,159 @@
+{-# LANGUAGE TypeInType, FlexibleInstances, MultiParamTypeClasses, TypeFamilies, AllowAmbiguousTypes, ScopedTypeVariables, TypeApplications, TypeOperators #-}
+
+module Instances where
+
+import Clr
+import Clr.Bridge
+import Clr.UnmarshalAs
+
+import Data.Int
+import Data.Word
+import Foreign.Ptr
+
+-- Just for testing. Actual use is BStr
+type instance BridgeTypePrim T_string = String
+type instance UnmarshalAs String = String
+
+-- Synonyms while we have to still write this manually
+type T_Console = T "System.Console" '[]
+type T_BaseType = T "BaseType" '[]
+type T_DerivedType = T "DerivedType" '[]
+
+-- Synonyms for methods
+type T_WriteLine = T "WriteLine" '[]
+type T_Foo = T "Foo" '[]
+type T_Bar = T "Bar" '[]
+type T_Add = T "Add" '[]
+
+type instance SuperTypes T_BaseType = '[ T_object ]
+type instance SuperTypes T_DerivedType = '[ T_BaseType ]
+type instance SuperTypes (T "MyGenType" '[gt0]) = '[ T_object, T "IEnumerable" '[], T "IEnumerable" '[gt0] ]
+
+type instance Members T_BaseType = '[T_Foo, T_Bar]
+type instance Members T_DerivedType = '[T_Foo]
+type instance Members (T "MyGenType" '[gt0]) = '[T_Add]
+
+type instance Candidates T_Console T_WriteLine = '[ '[T_string], '[T_string, T_string] ]
+type instance Candidates T_BaseType T_BaseType = '[ '[] ]
+type instance Candidates T_BaseType T_Foo = '[ '[ T_string ], '[ T_long ], '[ T_int ] ]
+type instance Candidates T_BaseType T_Bar = '[ '[ T_string ], '[ T_long ], '[ T_int ] ]
+type instance Candidates T_DerivedType T_DerivedType = '[ '[] ]
+type instance Candidates T_DerivedType T_Foo = '[ '[ T_string ], '[ T_long ], '[ T_int ]]
+type instance Candidates (T "MyGenType" '[gt0]) (T "MyGenType" '[gt0]) = '[ '[] ]
+type instance Candidates (T "MyGenType" '[gt0]) (T_Add) = '[ '[ gt0 ] ]
+
+writeLineRaw1 :: String -> IO String
+writeLineRaw1 cs = return "Console.WriteLine(String)"
+writeLineRaw2 :: String -> String -> IO String
+writeLineRaw2 cs1 cs2 = return "Console.WriteLine(String,String)"
+rawInvokeBaseTypeFooStr :: ObjectID t -> String -> IO String
+rawInvokeBaseTypeFooStr d s = return "BaseType.Foo(String)"
+rawInvokeBaseTypeFooInt64 :: ObjectID t -> Int64 -> IO String
+rawInvokeBaseTypeFooInt64 d s = return "BaseType.Foo(Int64)"
+rawInvokeBaseTypeFooInt32 :: ObjectID t -> Int32 -> IO String
+rawInvokeBaseTypeFooInt32 d s = return "BaseType.Foo(Int32)"
+rawInvokeDerivedTypeStr :: ObjectID t -> String -> IO String
+rawInvokeDerivedTypeStr d s = return "DerivedType.Foo(String)"
+rawInvokeDerivedTypeInt64 :: ObjectID t -> Int64 -> IO String
+rawInvokeDerivedTypeInt64 d s = return "DerivedType.Foo(Int64)"
+rawInvokeDerivedTypeInt32 :: ObjectID t -> Int32 -> IO String
+rawInvokeDerivedTypeInt32 d s = return "DerivedType.Foo(Int32)"
+rawInvokeBaseTypeBarStr :: ObjectID t -> String -> IO String
+rawInvokeBaseTypeBarStr d s = return "BaseType.Bar(String)"
+rawInvokeBaseTypeBarInt64 :: ObjectID t -> Int64 -> IO String
+rawInvokeBaseTypeBarInt64 d s = return "BaseType.Bar(Int64)"
+rawInvokeBaseTypeBarInt32 :: ObjectID t -> Int32 -> IO String
+rawInvokeBaseTypeBarInt32 d s = return "BaseType.Bar(Int32)"
+rawInvokeMyGenTypeAddStr :: ObjectID t -> String -> IO String
+rawInvokeMyGenTypeAddStr oid s = return "MyGenType.Add(String)"
+rawInvokeMyGenTypeAddInt :: ObjectID t -> Int32 -> IO String
+rawInvokeMyGenTypeAddInt oid s = return "MyGenType.Add(Int32)"
+
+
+instance MethodResultS1 T_Console T_WriteLine T_string where
+  type ResultTypeS1 T_Console T_WriteLine T_string = 'Just T_string
+
+instance MethodInvokeS1 T_Console T_WriteLine T_string where
+  rawInvokeS1 = writeLineRaw1
+
+instance MethodResultS2 T_Console T_WriteLine T_string T_string where
+  type ResultTypeS2 T_Console T_WriteLine T_string T_string = 'Just T_string
+
+instance MethodInvokeS2 T_Console T_WriteLine T_string T_string where
+  rawInvokeS2 = writeLineRaw2
+
+instance Constructor1 T_BaseType () where
+  rawNew1 () = return (ObjectID 1)
+
+instance MethodResultI1 T_BaseType T_Foo T_string where
+  type ResultTypeI1 T_BaseType T_Foo T_string = 'Just T_string
+
+instance MethodInvokeI1 T_BaseType T_Foo T_string where
+  rawInvokeI1 = rawInvokeBaseTypeFooStr
+
+instance MethodResultI1 T_BaseType T_Foo T_long where
+  type ResultTypeI1 T_BaseType T_Foo T_long = 'Just T_string
+
+instance MethodInvokeI1 T_BaseType T_Foo T_long where
+  rawInvokeI1 = rawInvokeBaseTypeFooInt64
+
+instance MethodResultI1 T_BaseType T_Foo T_int where
+  type ResultTypeI1 T_BaseType T_Foo T_int = 'Just T_string
+
+instance MethodInvokeI1 T_BaseType T_Foo T_int where
+  rawInvokeI1 = rawInvokeBaseTypeFooInt32
+
+instance MethodResultI1 T_BaseType T_Bar T_string where
+  type ResultTypeI1 T_BaseType T_Bar T_string = 'Just T_string
+
+instance MethodInvokeI1 T_BaseType T_Bar T_string where
+  rawInvokeI1 = rawInvokeBaseTypeBarStr
+
+instance MethodResultI1 T_BaseType T_Bar T_long where
+  type ResultTypeI1 T_BaseType T_Bar T_long = 'Just T_string
+
+instance MethodInvokeI1 T_BaseType T_Bar T_long where
+  rawInvokeI1 = rawInvokeBaseTypeBarInt64
+
+instance MethodResultI1 T_BaseType T_Bar T_int where
+  type ResultTypeI1 T_BaseType T_Bar T_int = 'Just T_string
+
+instance MethodInvokeI1 T_BaseType T_Bar T_int where
+  rawInvokeI1 = rawInvokeBaseTypeBarInt32
+
+instance Constructor1 T_DerivedType () where
+  rawNew1 () = return (ObjectID 1)
+
+instance MethodResultI1 T_DerivedType T_Foo T_string where
+  type ResultTypeI1 T_DerivedType T_Foo T_string = 'Just T_string
+
+instance MethodInvokeI1 T_DerivedType T_Foo T_string where
+  rawInvokeI1 = rawInvokeDerivedTypeStr
+
+instance MethodResultI1 T_DerivedType T_Foo T_long where
+  type ResultTypeI1 T_DerivedType T_Foo T_long = 'Just T_string
+
+instance MethodInvokeI1 T_DerivedType T_Foo T_long where
+  rawInvokeI1 = rawInvokeDerivedTypeInt64
+
+instance MethodResultI1 T_DerivedType T_Foo T_int where
+  type ResultTypeI1 T_DerivedType T_Foo T_int = 'Just T_string
+
+instance MethodInvokeI1 T_DerivedType T_Foo T_int where
+  rawInvokeI1 = rawInvokeDerivedTypeInt32
+
+instance Constructor1 (T "MyGenType" '[gt0]) () where
+  rawNew1 () = return (ObjectID 1)
+
+instance MethodResultI1 (T "MyGenType" '[T_string]) T_Add T_string where
+  type ResultTypeI1 (T "MyGenType" '[T_string]) T_Add T_string = 'Just T_string
+
+instance MethodInvokeI1 (T "MyGenType" '[T_string]) T_Add T_string where
+  rawInvokeI1 = rawInvokeMyGenTypeAddStr
+
+instance MethodResultI1 (T "MyGenType" '[T_int]) T_Add T_int where
+  type ResultTypeI1 (T "MyGenType" '[T_int]) T_Add T_int = 'Just T_string
+
+instance MethodInvokeI1 (T "MyGenType" '[T_int]) T_Add T_int where
+  rawInvokeI1 = rawInvokeMyGenTypeAddInt
+
--- libs/clr-typed/test/Spec.hs
+++ libs/clr-typed/test/Spec.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE TypeApplications, TypeInType, TypeFamilies #-}
 
+import Test.Hspec
+
 import Clr
-import Bindings()
+import Instances()
 
 import Data.Int(Int32, Int64)
 
--- not really a proper test suite but something that should compile with expected output that is manually looked at
-
+-- TODO: I used the next lines once for manual testing in GHCI. The order of finding the super type matters when looking up the base type
+-- Turn these next lines into something testable again.
 type instance SuperTypes (T "1" '[]) = '[]
 type instance SuperTypes (T "2" '[]) = '[ T "1" '[] ]
 type instance SuperTypes (T "3" '[]) = '[]
@@ -17,34 +19,31 @@ type instance SuperTypes (T "7" '[]) = '[]
 type instance SuperTypes (T "8" '[]) = '[ T "6" '[], T "7" '[], T "5" '[]]
 type instance SuperTypes (T "9" '[]) = '[ T "8" '[] ]
 
-
 main :: IO ()
 main = do
-  putStrLn ""
-  base <- new @"BaseType" ()                                 -- Constructors
-  derived <- new @"DerivedType" ()
-  putStrLn ""
-  invokeS @"WriteLine" @"System.Console" "Hi!"               -- Static method invocation
-  invokeS @"WriteLine" @"System.Console" ("Hello", "Again")  -- Overloaded
-  putStrLn ""
-  invokeI @"Foo" base "hi"                                   -- Instance method invocation
-  invokeI @"Foo" base (2::Int32)
-  invokeI @"Foo" base (2::Int64)
-  putStrLn ""
-  invokeI @"Foo" derived "hi"
-  invokeI @"Foo" derived (2::Int32)
-  invokeI @"Foo" derived (2::Int64)
-  putStrLn ""
-  invokeI @"Bar" base "hi"
-  invokeI @"Bar" base (2::Int32)
-  invokeI @"Bar" base (2::Int64)
-  putStrLn ""
-  invokeI @"Bar" derived "hi"                                -- DerivedType doesn't implement Bar so should call it on base type
-  invokeI @"Bar" derived (2::Int32)
-  invokeI @"Bar" derived (2::Int64)
-  putStrLn ""
-  myGenType <- new @'("MyGenType", "System.String") ()       -- Generic type
-  invokeI @"Add" myGenType "hello"
+  base <- new @"BaseType" ()                                 :: IO (Object (T "BaseType" '[]))                       -- Constructors
+  derived <- new @"DerivedType" ()                           :: IO (Object (T "DerivedType" '[]))
+
+  invokeS @"WriteLine" @"System.Console" "Hi!"              `shouldReturn` "Console.WriteLine(String)"               -- Static method invocation
+  invokeS @"WriteLine" @"System.Console" ("Hello", "Again") `shouldReturn` "Console.WriteLine(String,String)"        -- Overloaded
+
+  invokeI @"Foo" base "hi"                                  `shouldReturn` "BaseType.Foo(String)"                    -- Instance method invocation
+  invokeI @"Foo" base (2::Int32)                            `shouldReturn` "BaseType.Foo(Int32)"
+  invokeI @"Foo" base (2::Int64)                            `shouldReturn` "BaseType.Foo(Int64)"
+
+  invokeI @"Foo" derived "hi"                               `shouldReturn` "DerivedType.Foo(String)"
+  invokeI @"Foo" derived (2::Int32)                         `shouldReturn` "DerivedType.Foo(Int32)"
+  invokeI @"Foo" derived (2::Int64)                         `shouldReturn` "DerivedType.Foo(Int64)"
+
+  invokeI @"Bar" base "hi"                                  `shouldReturn` "BaseType.Bar(String)"
+  invokeI @"Bar" base (2::Int32)                            `shouldReturn` "BaseType.Bar(Int32)"
+  invokeI @"Bar" base (2::Int64)                            `shouldReturn` "BaseType.Bar(Int64)"
+
+  invokeI @"Bar" derived "hi"                               `shouldReturn` "BaseType.Bar(String)"                    -- DerivedType doesn't implement Bar so should call it on base type
+  invokeI @"Bar" derived (2::Int32)                         `shouldReturn` "BaseType.Bar(Int32)"
+  invokeI @"Bar" derived (2::Int64)                         `shouldReturn` "BaseType.Bar(Int64)"
+
+  myGenType <- new @'("MyGenType", "System.String") ()       :: IO (Object (T "MyGenType" '[T "System.String" '[]])) -- Generic type
+  invokeI @"Add" myGenType "hello"                          `shouldReturn` "MyGenType.Add(String)"
 --  invokeI @"Add" myGenType (2::Int32)                      -- This would be a compilation error
-  putStrLn ""
 
